2ba53f223988152136a4bed042b1e273c275e227
Christinelee1 <45888829+Christinelee1@users.noreply.github.com>
2019-06-12 10:53:27 +0800

    Update MyDataBase.java

--- java-chat-master/MyDataBase.java
+++ java-chat-master/MyDataBase.java
@@ -11,10 +11,31 @@ public class MyDataBase {
 	
 	//数据库
 	static{
-		list.add("手#一種人體部位，1個字");
+		list.add("apple#English，5 words");
+		list.add("stool#English，5 words");
+		list.add("universe#English，8 words");
+		list.add("desert#English，6 words");
+		list.add("dream#English，5 words");
+		list.add("hand#English，4 words");
+		list.add("ticket#English，6 words");
+		list.add("turkey#English，6 words");
+		list.add("champion#English，8 words");
+		list.add("rocket#English，6 words");
+		list.add("computer#English，8 words");
+		list.add("rice#English，4 words");
+		list.add("TV#English，2 words");
+		list.add("eclipse#English，6 words");
+		list.add("earthquake#English，10 words");
+		list.add("typhoon#English，7 words");
+		list.add("hurricane#English，9 words");
+		list.add("SpongeBob#English，9 words");
+		list.add("Teletubbies#English，11 words");
+		list.add("Teletubbies#English，11 words");
+		
+		/*list.add("手#一種人體部位，1個字");
 		list.add("蘋果#一種水果，2個字");
 		list.add("大便#虹婷喜歡的東西，2個字");
-		/*list.add("菜鸟#一种动物，两个字");
+		list.add("菜鸟#一种动物，两个字");
 		list.add("大树#一种植物，两个字");
 		list.add("猪#一种动物，一个字");
 		list.add("葡萄#一种水果，两个字");
